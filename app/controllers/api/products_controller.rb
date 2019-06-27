d8c5dbed6f23f6a3157020cbf136afab69e179bc
Sim <simchasitzer1@gmail.com>
2019-06-26 22:35:34 -0400

    make show any product route using query url and segment url

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,5 +1,17 @@
 class Api::ProductsController < ApplicationController
   def show_products
+    @products = Product.all
     render 'products.json.jb'
+  end
+
+  def iphone_deets
+    @iphone = Product.find_by(name: "iPhone XS Max")
+    render 'iphone.json.jb'
+  end
+
+  def show_any_product
+    product = params["id"]
+    @info = Product.find_by(id: product)
+    render 'show_a_product.json.jb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     get "/products" => "products#show_products"
+    get "/iphone" => "products#iphone_deets"
+    get 'show_any_product_query' => 'products#show_any_product'
+    get 'show_any_product_segment/:id' => 'products#show_any_product'
   end
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
