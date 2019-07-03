2eec2fe1d9702d97add7fdd95ceff8e804857aee
Sim <simchasitzer1@gmail.com>
2019-07-02 20:08:18 -0400

    add Product model methods, formatted model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,29 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    if self.price < 10
+      the_boolean = true  
+    else
+      the_boolean = false
+    end
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def full_price
+    price + (price * 0.09)
+  end
+
+  def formatted_price
+    "$#{price}"
+  end
+
+  def formatted_tax
+    "$#{tax}"
+  end
+
+  def formatted_full_price
+    "$#{full_price}"
+  end
 end
