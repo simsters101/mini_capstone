c916835fcfa0a2d6a713e4a651cb15ae252dbf94
Sim <simchasitzer1@gmail.com>
2019-07-18 20:50:03 -0400

    add create and new action for products, add update and edit actions for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,47 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+    render 'index.html.erb'
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
     render 'show.html.erb'
+  end
+
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      description: params[:description],
+      supplier_id: params[:supplier_id]
+    )
+
+    @product.save
+
+    redirect_to "/products/#{@product.id}"
+  end
+
+  def new
+    render 'new.html.erb'
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.description = params[:description] || @product.description
+    @product.supplier_id = params[:supplier_id] || @product.supplier_id
+    @product.save
+    if @product.save
+      redirect_to "/products/#{@product.id}"
+    else
+      render 'errors.json.jb', status: :unprocessible_entity
+    end
+  end
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+    render 'edit.html.erb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,14 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   namespace :api do
-    get 'products' => 'products#index'
-    post 'products' => 'products#create'
-    get 'show_query' => 'products#show'
-    get 'products/:id' => 'products#show'
-    patch 'products/:id' => 'products#update'
-    delete 'products/:id' => 'products#destroy'
+    get '/products' => 'products#index'
+    post '/products' => 'products#create'
+    get '/show_query' => 'products#show'
+    get '/products/:id' => 'products#show'
+    patch '/products/:id' => 'products#update'
+    delete '/products/:id' => 'products#destroy'
 
-    get 'suppliers' => 'suppliers#index'
+    get '/suppliers' => 'suppliers#index'
 
     post '/users' => 'users#create'
 
@@ -20,13 +20,18 @@ Rails.application.routes.draw do
     get '/orders' => 'orders#index'
     get '/orders/:id' => 'orders#show'
 
-    post 'carted_products' => 'carted_products#create'
-    get 'carted_products' => 'carted_products#index'
-    get 'carted_products/:id' => 'carted_products#show'
-    delete 'carted_products/:id' => 'carted_products#destroy'
+    post '/carted_products' => 'carted_products#create'
+    get '/carted_products' => 'carted_products#index'
+    get '/carted_products/:id' => 'carted_products#show'
+    delete '/carted_products/:id' => 'carted_products#destroy'
   end
 
   get '/products' => 'products#index'
+  get '/products/new' => 'products#new'
+  post '/products' => 'products#create'
+  get '/products/:id' => 'products#show'
+  get '/products/:id/edit' => 'products#edit'
+  patch '/products/:id' => 'products#update'
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   # namespace :api do
